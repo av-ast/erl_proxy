d07cff3429c8358e826d693800067ac558385c24
Alexey Astafyev <av.astafyev@gmail.com>
2013-11-29 17:58:53 +0400

    added util function convertion list of binaries to list of lists

--- src/utils.erl
+++ src/utils.erl
@@ -1,7 +1,18 @@
 -module(utils).
 
--export([ts/0]).
+-export([ts/0, deep_binary_to_list/1]).
 
 ts() ->
   {Megasecs, Secs, Microsecs} = now(),
   Microsecs + 1000000 * (Secs + 1000000 * Megasecs).
+
+%%
+%% @spec deep_binary_to_list(List) -> list() | bad_arg
+deep_binary_to_list([]) -> [];
+deep_binary_to_list([{Key, Value}|Tail]) when is_atom(Key), is_binary(Value) ->
+  [{Key, binary_to_list(Value)} | deep_binary_to_list(Tail)];
+deep_binary_to_list([{Key, [{V1, V2}|_] = Pairs}|Tail]) when is_atom(Key), is_binary(V1), is_binary(V2) ->
+  NewValue = [{binary_to_list(Val1), binary_to_list(Val2)} || {Val1, Val2} <- Pairs],
+  [{Key, NewValue} | deep_binary_to_list(Tail)];
+deep_binary_to_list(_) ->
+  bad_arg.
