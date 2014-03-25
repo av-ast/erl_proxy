780bee49b1cc487b40682712194d13fc9f345219
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2014-03-25 16:24:09 +0400

    use hierdis

--- src/schedule.erl
+++ src/schedule.erl
@@ -17,7 +17,7 @@ start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-  {ok, RedisClient} = eredis:start_link(),
+  {ok, RedisClient} = hierdis:connect("127.0.0.1", 6379),
   {ok, #state{redis_client = RedisClient}}.
 
 stop() ->
@@ -61,31 +61,33 @@ length() ->
 %% gen_server callbacks
 
 handle_call(clear, _From, #state{redis_client = RedisClient} = State) ->
-  {ok, _} = eredis:q(RedisClient, ["DEL", sorted_set_key_name()]),
-  {ok, _} = eredis:q(RedisClient, ["DEL", hash_key_name()]),
+  {ok, _} = hierdis:command(RedisClient, ["DEL", sorted_set_key_name()]),
+  {ok, _} = hierdis:command(RedisClient, ["DEL", hash_key_name()]),
   {reply, ok, State};
 
 handle_call({retrieve, Timestamp}, _From, #state{redis_client = RedisClient} = State) ->
-  Result = case eredis:q(RedisClient, ["ZRANGEBYSCORE", sorted_set_key_name(), "-inf", Timestamp, "LIMIT", 0, 1]) of
+  TimestampStr = integer_to_list(Timestamp),
+  Result = case hierdis:command(RedisClient, ["ZRANGEBYSCORE", sorted_set_key_name(), "-inf", TimestampStr, "LIMIT", "0", "1"]) of
     {ok, []} -> nothing;
-    {ok, [TermId]} ->
-      {ok, <<"1">>} = eredis:q(RedisClient, ["ZREM", sorted_set_key_name(), TermId]),
-      {ok, Res} = eredis:q(RedisClient, ["HGET", hash_key_name(), TermId]),
-      {ok, <<"1">>} = eredis:q(RedisClient, ["HDEL", hash_key_name(), TermId]),
+    {ok, [TermIdStr]} ->
+      {ok, 1} = hierdis:command(RedisClient, ["ZREM", sorted_set_key_name(), TermIdStr]),
+      {ok, Res} = hierdis:command(RedisClient, ["HGET", hash_key_name(), TermIdStr]),
+      {ok, 1} = hierdis:command(RedisClient, ["HDEL", hash_key_name(), TermIdStr]),
       binary_to_term(Res)
   end,
 
   {reply, Result, State};
 handle_call(length, _From, #state{redis_client = RedisClient} = State) ->
-  {ok, Length} = eredis:q(RedisClient, ["ZCOUNT", sorted_set_key_name(), "-inf", "+inf"]),
-  {reply, binary_to_integer(Length), State};
+  {ok, Length} = hierdis:command(RedisClient, ["ZCOUNT", sorted_set_key_name(), "-inf", "+inf"]),
+  {reply, Length, State};
 handle_call(_Message, _From, State) ->
   {reply, error, State}.
 
 handle_cast({add, Term, PerformAt}, #state{redis_client = RedisClient} = State) ->
-  TermId = utils:ts(),
-  {ok, <<"1">>} = eredis:q(RedisClient, ["HSET", hash_key_name(), TermId, Term]),
-  {ok, <<"1">>} = eredis:q(RedisClient, ["ZADD", sorted_set_key_name(), PerformAt, TermId]),
+  TermIdStr = integer_to_list(utils:ts()),
+  PerformAtStr = integer_to_list(PerformAt),
+  {ok, 1} = hierdis:command(RedisClient, ["HSET", hash_key_name(), TermIdStr, Term]),
+  {ok, 1} = hierdis:command(RedisClient, ["ZADD", sorted_set_key_name(), PerformAtStr, TermIdStr]),
   {noreply, State};
 handle_cast(stop, State) ->
   {stop, normal, State};
