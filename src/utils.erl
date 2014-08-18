2f23500a32c86239ab4e4891fa078acfe1ed61ad
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2014-08-18 12:14:25 +0400

    add suport for old erlang

--- src/utils.erl
+++ src/utils.erl
@@ -22,7 +22,7 @@ ip_string_from_cowboy_req(Req) ->
 
 % return reversed list
 redis_withscores_to_tuple_list([Member, Score | T]) ->
-  ScoreInt = binary_to_integer(Score),
+  ScoreInt = list_to_integer(binary_to_list(Score)),
   [{Member, ScoreInt} | redis_withscores_to_tuple_list(T)];
 redis_withscores_to_tuple_list(_) ->
   [].
